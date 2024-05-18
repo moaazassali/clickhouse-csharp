73ab257cae56c38a7d5364eabe1ca7c2e2ac15c1
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-18 20:39:35 +0400

    Added temporary test project to quickly check library

---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClickHouse.Connector.Connector;
+
+namespace Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var connection = new ClickHouseConnection("192.168.70.176");
+        var serverInfo = connection.GetServerInfo();
+        Console.WriteLine(serverInfo);
+        Console.WriteLine(serverInfo.Name);
+        Console.WriteLine(serverInfo.Timezone);
+        Console.WriteLine(serverInfo.DisplayName);
+        Console.WriteLine(serverInfo.VersionMinor);
+        Console.WriteLine(serverInfo.VersionMajor);
+        Console.WriteLine(serverInfo.VersionPatch);
+        Console.WriteLine(serverInfo.Revision);
+    }
+}
