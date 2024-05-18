e9c0bce9cf75cc96d0a7669f8351944d0c7d92fe
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-18 20:31:09 +0400

    renamed external dll

--- ClickHouse.Connector/Native/NativeClient.cs
+++ ClickHouse.Connector/Native/NativeClient.cs
@@ -5,13 +5,13 @@ namespace ClickHouse.Connector.Native;
 
 internal static partial class NativeClient
 {
-    [LibraryImport("clickhouse-cpp-wrapper.dll", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll", StringMarshalling = StringMarshalling.Utf8)]
     public static partial nint CreateClient(string host);
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll")]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll")]
     public static partial void FreeClient(nint client);
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll", StringMarshalling = StringMarshalling.Utf8)]
     public static partial NativeClickHouseResultStatus Execute(nint client, nint query);
 
     // Select
@@ -19,22 +19,23 @@ internal static partial class NativeClient
     // SelectCancelable
     // SelectCancelableWithQueryId
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll", StringMarshalling = StringMarshalling.Utf8)]
     public static partial NativeClickHouseResultStatus Insert(nint client, string tableName, nint block);
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll", StringMarshalling = StringMarshalling.Utf8)]
     public static partial void InsertWithQueryId(nint client, string tableName, string queryId, nint block);
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll")]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll")]
     public static partial void Ping(nint client);
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll")]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll")]
     public static partial void ResetConnection(nint client);
 
-    // GetServerInfo
+    [LibraryImport("clickhouse-cpp-c-bridge.dll")]
+    public static partial NativeServerInfo GetServerInfo(nint client);
     // GetCurrentEndpoint
 
-    [LibraryImport("clickhouse-cpp-wrapper.dll")]
+    [LibraryImport("clickhouse-cpp-c-bridge.dll")]
     public static partial void ResetConnectionEndpoint(nint client);
 
     // GetVersion
