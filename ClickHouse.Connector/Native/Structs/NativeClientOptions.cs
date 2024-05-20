53479f0f0db36de258610dbaded3cd6f824e5286
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-05-20 17:57:31 +0400

    Added support for client options struct

---
+++ ClickHouse.Connector/Native/Structs/NativeClientOptions.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace ClickHouse.Connector.Native.Structs;
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeClientOptions
+{
+    internal nint Host;
+    internal ushort Port;
+    internal nint Endpoints;
+    internal nuint EndpointsCount;
+    internal nint DefaultDatabase;
+    internal nint User;
+    internal nint Password;
+    internal byte RethrowExceptions;
+    internal byte PingBeforeQuery;
+    internal uint SendRetries;
+    internal long RetryTimeout;
+    internal int CompressionMethod;
+    internal byte TcpKeepalive;
+    internal long TcpKeepaliveIdle;
+    internal long TcpKeepaliveIntvl;
+    internal uint TcpKeepaliveCnt;
+    internal byte TcpNodelay;
+    internal long ConnectionConnectTimeout;
+    internal long ConnectionRecvTimeout;
+    internal long ConnectionSendTimeout;
+    internal byte BackwardCompatibilityLowcardinalityAsWrappedColumn;
+    internal uint MaxCompressionChunkSize;
+
+    internal void Free(NativeEndpoint[] nativeEndpoints)
+    {
+        Marshal.FreeHGlobal(Host);
+
+        foreach (var nativeEndpoint in nativeEndpoints)
+        {
+            nativeEndpoint.Free();
+        }
+
+        Marshal.FreeHGlobal(Endpoints);
+        Marshal.FreeHGlobal(DefaultDatabase);
+        Marshal.FreeHGlobal(User);
+        Marshal.FreeHGlobal(Password);
+    }
+}
