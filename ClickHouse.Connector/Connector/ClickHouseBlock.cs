44cdf4ade693470c850185edebc19d01a79e0e8c
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-06-13 22:33:38 +0400

    Updated ClickHouseBlock to add native columns as library columns to its columns list

--- ClickHouse.Connector/Connector/ClickHouseBlock.cs
+++ ClickHouse.Connector/Connector/ClickHouseBlock.cs
@@ -8,16 +8,33 @@ public class ClickHouseBlock : IDisposable
     private bool _disposed;
     public List<ClickHouseColumn> Columns { get; } = [];
 
+    // again, possible conversion loss from nuint to int
+    // will anyone have more than 2B rows in one block?
+    public int RowCount
+    {
+        get
+        {
+            CheckDisposed();
+            return (int)Native.NativeBlock.GetRowCount(NativeBlock);
+        }
+    }
+
     public ClickHouseBlock()
     {
         NativeBlock = Native.NativeBlock.CreateBlock();
         _disposed = false;
     }
-    
+
     public ClickHouseBlock(nint nativeBlock)
     {
         NativeBlock = nativeBlock;
         _disposed = false;
+
+        for (nuint i = 0; i < Native.NativeBlock.GetColumnCount(nativeBlock); i++)
+        {
+            var nativeColumn = Native.NativeBlock.GetColumnAt(nativeBlock, i);
+            Columns.Add(CreateColumn(Native.Columns.NativeColumn.GetColumnType(nativeColumn), nativeColumn));
+        }
     }
 
     public void Dispose()
@@ -45,18 +62,6 @@ public class ClickHouseBlock : IDisposable
         Native.NativeBlock.AppendColumn(NativeBlock, tableName, column.NativeColumn);
     }
 
-    public nuint GetColumnCount()
-    {
-        CheckDisposed();
-        return Native.NativeBlock.GetColumnCount(NativeBlock);
-    }
-
-    public nuint GetRowCount()
-    {
-        CheckDisposed();
-        return Native.NativeBlock.GetRowCount(NativeBlock);
-    }
-
     public nuint RefreshRowCount()
     {
         CheckDisposed();
@@ -67,5 +72,48 @@ public class ClickHouseBlock : IDisposable
     {
         CheckDisposed();
         return Native.NativeBlock.GetColumnName(NativeBlock, index);
+    }
+
+    private static ClickHouseColumn CreateColumn(ClickHouseColumnType type, nint nativeColumn)
+    {
+        return type switch
+        {
+            ClickHouseColumnType.Int8 => new ClickHouseColumnInt8(nativeColumn),
+            ClickHouseColumnType.Int16 => new ClickHouseColumnInt16(nativeColumn),
+            ClickHouseColumnType.Int32 => new ClickHouseColumnInt32(nativeColumn),
+            ClickHouseColumnType.Int64 => new ClickHouseColumnInt64(nativeColumn),
+            ClickHouseColumnType.UInt8 => new ClickHouseColumnUInt8(nativeColumn),
+            ClickHouseColumnType.UInt16 => new ClickHouseColumnUInt16(nativeColumn),
+            ClickHouseColumnType.UInt32 => new ClickHouseColumnUInt32(nativeColumn),
+            ClickHouseColumnType.UInt64 => new ClickHouseColumnUInt64(nativeColumn),
+            ClickHouseColumnType.Float32 => new ClickHouseColumnFloat32(nativeColumn),
+            ClickHouseColumnType.Float64 => new ClickHouseColumnFloat64(nativeColumn),
+            ClickHouseColumnType.String => new ClickHouseColumnString(nativeColumn),
+            // ClickHouseColumnType.FixedString => new ClickHouseColumnFixedString(),
+            // ClickHouseColumnType.DateTime => new ClickHouseColumnDateTime(),
+            // ClickHouseColumnType.Date => new ClickHouseColumnDate(),
+            // ClickHouseColumnType.Array => new ClickHouseColumnArray(),
+            // ClickHouseColumnType.Nullable => new ClickHouseColumnNullable(),
+            // ClickHouseColumnType.Tuple => new ClickHouseColumnTuple(),
+            // ClickHouseColumnType.Enum8 => new ClickHouseColumnEnum8(),
+            // ClickHouseColumnType.Enum16 => new ClickHouseColumnEnum16(),
+            // ClickHouseColumnType.Uuid => new ClickHouseColumnUuid(),
+            // ClickHouseColumnType.IPv4 => new ClickHouseColumnIPv4(),
+            // ClickHouseColumnType.IPv6 => new ClickHouseColumnIPv6(),
+            // ClickHouseColumnType.Int128 => new ClickHouseColumnInt128(),
+            // ClickHouseColumnType.Decimal => new ClickHouseColumnDecimal(),
+            // ClickHouseColumnType.Decimal32 => new ClickHouseColumnDecimal32(),
+            // ClickHouseColumnType.Decimal64 => new ClickHouseColumnDecimal64(),
+            // ClickHouseColumnType.Decimal128 => new ClickHouseColumnDecimal128(),
+            // ClickHouseColumnType.LowCardinality => new ClickHouseColumnLowCardinality(),
+            ClickHouseColumnType.DateTime64 => new ClickHouseColumnDateTime64(nativeColumn),
+            // ClickHouseColumnType.Date32 => new ClickHouseColumnDate32(),
+            // ClickHouseColumnType.Map => new ClickHouseColumnMap(),
+            // ClickHouseColumnType.Point => new ClickHouseColumnPoint(),
+            // ClickHouseColumnType.Ring => new ClickHouseColumnRing(),
+            // ClickHouseColumnType.Polygon => new ClickHouseColumnPolygon(),
+            // ClickHouseColumnType.MultiPolygon => new ClickHouseColumnMultiPolygon(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
     }
 }
