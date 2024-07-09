05e0c2372ebf2df7684a2bd98cbbfd287a9c94f8
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 02:33:07 +0400

    added column int128 tests

--- ClickHouse.Driver.Tests/Unit/Columns/ColumnInt128Tests.cs
+++ ClickHouse.Driver.Tests/Unit/Columns/ColumnInt128Tests.cs
@@ -1,6 +1,19 @@
+using ClickHouse.Driver.Columns;
+
 namespace ClickHouse.Driver.Tests.Unit.Columns;
 
 public class ColumnInt128Tests
 {
+    private readonly ColumnInt128 _column = new();
 
+    [Fact]
+    public void Add_SingleValue_ReturnsSameValue()
+    {
+        var value = new Int128(123, 456);
+
+        _column.Add(value);
+        var actual = _column[0];
+
+        Assert.Equal(value, actual);
+    }
 }
