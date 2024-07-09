41f940a8ab6f940d3e1af8989fbf2a2ed3fec768
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 00:55:32 +0400

    added to/from conversion for int128interop to int128

--- ClickHouse.Driver/Interop/Structs/Int128Interop.cs
+++ ClickHouse.Driver/Interop/Structs/Int128Interop.cs
@@ -7,4 +7,9 @@ internal struct Int128Interop
 {
     internal long High;
     internal ulong Low;
+
+    internal Int128 ToInt128() => ((Int128)High << 64) | Low;
+
+    internal static Int128Interop FromInt128(Int128 value) =>
+        new() { High = unchecked((long)(value >> 64)), Low = (ulong)value };
 }
