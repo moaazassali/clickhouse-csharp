9e55298926c74d30d01c9d2574de3f36751f4283
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-09 20:46:34 +0400

    added reserve, clear and size methods to column

--- ClickHouse.Driver/Columns/ClickHouseColumn.cs
+++ ClickHouse.Driver/Columns/ClickHouseColumn.cs
@@ -15,6 +15,27 @@ public abstract class ClickHouseColumn : IDisposable
         }
     }
 
+    public void Reserve(int size)
+    {
+        CheckDisposed();
+        Interop.Columns.ColumnInterop.chc_column_reserve(NativeColumn, (nuint)size);
+    }
+
+    public void Clear()
+    {
+        CheckDisposed();
+        Interop.Columns.ColumnInterop.chc_column_clear(NativeColumn);
+    }
+
+    public int Size
+    {
+        get
+        {
+            CheckDisposed();
+            return (int)Interop.Columns.ColumnInterop.chc_column_size(NativeColumn);
+        }
+    }
+
     public void Dispose()
     {
         Interop.Columns.ColumnInterop.chc_column_free(NativeColumn);
