aa9ce12405f96dd1d8372f9f416488869bdf8dac
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 23:24:20 +0400

    added new column method for type name

--- ClickHouse.Driver/Interop/Columns/ColumnInterop.cs
+++ ClickHouse.Driver/Interop/Columns/ColumnInterop.cs
@@ -9,7 +9,10 @@ internal static partial class ColumnInterop
     public static partial void chc_column_free(nint column);
 
     [LibraryImport("clickhouse-cpp-c-bridge")]
-    public static partial ColumnType chc_column_type(nint column);
+    public static partial ColumnType chc_column_type_code(nint column);
+
+    [LibraryImport("clickhouse-cpp-c-bridge", StringMarshalling = StringMarshalling.Utf8)]
+    public static partial string chc_column_type_name(nint column);
 
     [LibraryImport("clickhouse-cpp-c-bridge")]
     public static partial void chc_column_reserve(nint column, nuint size);
