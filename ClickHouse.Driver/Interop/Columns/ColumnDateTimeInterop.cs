e78865affcd12aab87877b2864d342b99175eec6
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 16:33:09 +0400

    Update ColumnDateTimeInterop.cs

--- ClickHouse.Driver/Interop/Columns/ColumnDateTimeInterop.cs
+++ ClickHouse.Driver/Interop/Columns/ColumnDateTimeInterop.cs
@@ -11,5 +11,5 @@ internal static partial class ColumnDateTimeInterop
     public static partial void chc_column_datetime_append(nint column, uint value);
 
     [LibraryImport("clickhouse-cpp-c-bridge")]
-    public static partial int chc_column_datetime_at(nint column, nuint index);
+    public static partial uint chc_column_datetime_at(nint column, nuint index);
 }
