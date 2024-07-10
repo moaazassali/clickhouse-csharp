d0d9a72e1b86a7288c561dcdfbcef399789702a6
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 17:04:01 +0400

    Create ColumnEnum8Tests.cs

---
+++ ClickHouse.Driver.Tests/Unit/Columns/ColumnEnum8Tests.cs
@@ -0,0 +1,29 @@
+using ClickHouse.Driver.Columns;
+
+namespace ClickHouse.Driver.Tests.Unit.Columns;
+
+public class ColumnEnum8Tests
+{
+    [Fact]
+    public void Constructor_ThrowsException_WhenEnumTypeIsNotSByte()
+    {
+        Assert.Throws<InvalidOperationException>(() => new ColumnEnum8<DayOfWeek>());
+    }
+
+    private enum DayOfWeek2 : sbyte
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+
+    [Fact]
+    public void Constructor_DoesNotThrowException_WhenEnumTypeIsSByte()
+    {
+        var col = new ColumnEnum8<DayOfWeek2>();
+    }
+}
