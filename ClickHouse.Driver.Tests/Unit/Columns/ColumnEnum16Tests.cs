c59ce3eb60736c79ae7180e037e339b10d1cbbea
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-10 17:03:59 +0400

    Create ColumnEnum16Tests.cs

---
+++ ClickHouse.Driver.Tests/Unit/Columns/ColumnEnum16Tests.cs
@@ -0,0 +1,29 @@
+using ClickHouse.Driver.Columns;
+
+namespace ClickHouse.Driver.Tests.Unit.Columns;
+
+public class ColumnEnum16Tests
+{
+    [Fact]
+    public void Constructor_ThrowsException_WhenEnumTypeIsNotShort()
+    {
+        Assert.Throws<InvalidOperationException>(() => new ColumnEnum16<DayOfWeek>());
+    }
+
+    private enum DayOfWeek2 : short
+    {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
+
+    [Fact]
+    public void Constructor_DoesNotThrowException_WhenEnumTypeIsShort()
+    {
+        var col = new ColumnEnum16<DayOfWeek2>();
+    }
+}
