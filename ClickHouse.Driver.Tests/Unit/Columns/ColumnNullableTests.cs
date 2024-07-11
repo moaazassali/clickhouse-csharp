82712ebed3f934e9e4952495e92e43ccadfeb1b7
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-11 20:31:21 +0400

    Create ColumnNullableTests.cs

---
+++ ClickHouse.Driver.Tests/Unit/Columns/ColumnNullableTests.cs
@@ -0,0 +1,12 @@
+using ClickHouse.Driver.Columns;
+
+namespace ClickHouse.Driver.Tests.Unit.Columns;
+
+public class ColumnNullableTests
+{
+    [Fact]
+    public void Test()
+    {
+        // var col = new ColumnNullable<ColumnInt8>();
+    }
+}
