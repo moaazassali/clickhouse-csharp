a10144b216569085e7f58ce2320f58b582a2037b
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-18 23:10:12 +0400

    Update ColumnNullableTests.cs

--- ClickHouse.Driver.Tests/Unit/Columns/ColumnNullableTests.cs
+++ ClickHouse.Driver.Tests/Unit/Columns/ColumnNullableTests.cs
@@ -5,8 +5,22 @@ namespace ClickHouse.Driver.Tests.Unit.Columns;
 public class ColumnNullableTests
 {
     [Fact]
-    public void Test()
+    public void Add_NonNullValue_ReturnsSameValue()
     {
-        // var col = new ColumnNullable<ColumnInt8>();
+        var col = new Column<ChNullable<ChInt8>>();
+
+        col.Add((ChInt8)42);
+
+        Assert.Equal(42, col[0].Value);
+    }
+
+    [Fact]
+    public void Add_NullValue_ReturnsNull()
+    {
+        var col = new Column<ChNullable<ChInt8>>();
+
+        col.Add(null);
+
+        Assert.False(col[0].HasValue);
     }
 }
