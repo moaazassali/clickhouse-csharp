573a7ccea7ae3baa724f9bcc60eadea3a2341836
Moaaz Assali <66834697+moaazassali@users.noreply.github.com>
2024-07-22 17:36:31 +0400

    Update Program.cs

--- ClickHouse.Driver.Benchmarks/Program.cs
+++ ClickHouse.Driver.Benchmarks/Program.cs
@@ -1,4 +1,10 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using ClickHouse.Driver.Columns;
 
 namespace ClickHouse.Driver.Benchmarks;
 
@@ -6,6 +12,12 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
+        var config = DefaultConfig.Instance
+            .AddJob(Job
+                .MediumRun
+                .WithToolchain(InProcessNoEmitToolchain.Instance)
+            );
         BenchmarkRunner.Run<InsertBenchmark>();
+        // BenchmarkRunner.Run<QueryBenchmark>(config);
     }
 }
